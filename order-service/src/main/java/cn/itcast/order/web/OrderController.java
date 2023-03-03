891f33955d7849d56b5c3633199807bbc52af3be
yepianer <aimeinvshen@126.com>
2023-03-03 16:15:45 +0800

    服务拆分 远程服务调用

--- order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -1,12 +1,14 @@
 package cn.itcast.order.web;
 
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import cn.itcast.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("order")
@@ -15,9 +17,19 @@ public class OrderController {
    @Autowired
    private OrderService orderService;
 
+   @Autowired
+   private RestTemplate restTemplate;
     @GetMapping("{orderId}")
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
+
+        Order order = orderService.queryOrderById(orderId);
+        //url路径
+        String url = "http://localhost:8081/user/" + order.getUserId();
+        //发送http请求，实现远程调用
+        User user = restTemplate.getForObject(url, User.class);
+        //封装user到Order
+        order.setUser(user);
         // 根据id查询订单并返回
-        return orderService.queryOrderById(orderId);
+        return order;
     }
 }
