bf900fccb8366aacbcab5d27e75b97f6c545af6d
yepianer <aimeinvshen@126.com>
2023-03-06 21:46:45 +0800

    Feign 基于Feign远程调用

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
@@ -13,6 +14,18 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
     @Autowired
+    private UserClient userClient;
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        //2.用Feign远程调用
+        User user = userClient.findById(order.getUserId());
+        //3.封装user到Order
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }
+    /*@Autowired
     private RestTemplate restTemplate;
 
     public Order queryOrderById(Long orderId) {
@@ -27,5 +40,5 @@ public class OrderService {
         order.setUser(user);
         // 4.返回
         return order;
-    }
+    }*/
 }
